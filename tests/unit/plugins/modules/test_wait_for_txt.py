31a8c3c5f60e7405338b4a52bd20943aa10532b2
Felix Fontein <felix@fontein.de>
2021-03-03 08:53:40 +0100

    Test error situations.

--- tests/unit/plugins/modules/test_wait_for_txt.py
+++ tests/unit/plugins/modules/test_wait_for_txt.py
@@ -456,7 +456,6 @@ class TestWaitForTXT(ModuleTestCase):
                                         'mode': 'subset',
                                     },
                                 ],
-                                'timeout': 10,
                             })
                             wait_for_txt.main()
 
@@ -609,7 +608,6 @@ class TestWaitForTXT(ModuleTestCase):
                                         'mode': 'superset',
                                     },
                                 ],
-                                'timeout': 10,
                             })
                             wait_for_txt.main()
 
@@ -732,7 +730,6 @@ class TestWaitForTXT(ModuleTestCase):
                                         'mode': 'superset_not_empty',
                                     },
                                 ],
-                                'timeout': 10,
                             })
                             wait_for_txt.main()
 
@@ -854,7 +851,6 @@ class TestWaitForTXT(ModuleTestCase):
                                         'mode': 'equals',
                                     },
                                 ],
-                                'timeout': 10,
                             })
                             wait_for_txt.main()
 
@@ -977,7 +973,6 @@ class TestWaitForTXT(ModuleTestCase):
                                         'mode': 'equals_ordered',
                                     },
                                 ],
-                                'timeout': 10,
                             })
                             wait_for_txt.main()
 
@@ -1151,3 +1146,215 @@ class TestWaitForTXT(ModuleTestCase):
         assert exc.value.args[0]['records'][1]['done'] is True
         assert exc.value.args[0]['records'][1]['values'] == ['any bar']
         assert exc.value.args[0]['records'][1]['check_count'] == 1
+
+    def test_nxdomain(self):
+        resolver = mock_resolver(['1.1.1.1'], {})
+        udp_sequence = [
+            {
+                'query_target': dns.name.from_unicode(u'com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NXDOMAIN),
+            },
+        ]
+        with patch('dns.resolver.get_default_resolver', resolver):
+            with patch('dns.resolver.Resolver', resolver):
+                with patch('dns.query.udp', mock_query_udp(udp_sequence)):
+                    with patch('time.sleep', mock_sleep):
+                        with pytest.raises(AnsibleFailJson) as exc:
+                            set_module_args({
+                                'records': [
+                                    {
+                                        'name': 'www.example.com',
+                                        'values': [
+                                            'asdf',
+                                        ],
+                                    },
+                                ],
+                            })
+                            wait_for_txt.main()
+
+        print(exc.value.args[0])
+        assert exc.value.args[0]['msg'] == 'Unexpected DNS error: The DNS query name does not exist: com.'
+        assert exc.value.args[0]['completed'] == 0
+        assert len(exc.value.args[0]['records']) == 1
+        assert exc.value.args[0]['records'][0]['name'] == 'www.example.com'
+        assert exc.value.args[0]['records'][0]['done'] is False
+        assert 'values' not in exc.value.args[0]['records'][0]
+        assert exc.value.args[0]['records'][0]['check_count'] == 0
+
+    def test_servfail(self):
+        resolver = mock_resolver(['1.1.1.1'], {})
+        udp_sequence = [
+            {
+                'query_target': dns.name.from_unicode(u'com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.SERVFAIL),
+            },
+        ]
+        with patch('dns.resolver.get_default_resolver', resolver):
+            with patch('dns.resolver.Resolver', resolver):
+                with patch('dns.query.udp', mock_query_udp(udp_sequence)):
+                    with patch('time.sleep', mock_sleep):
+                        with pytest.raises(AnsibleFailJson) as exc:
+                            set_module_args({
+                                'records': [
+                                    {
+                                        'name': 'www.example.com',
+                                        'values': [
+                                            'asdf',
+                                        ],
+                                    },
+                                ],
+                            })
+                            wait_for_txt.main()
+
+        print(exc.value.args[0])
+        assert exc.value.args[0]['msg'] == 'Unexpected resolving error: Error SERVFAIL'
+        assert exc.value.args[0]['completed'] == 0
+        assert len(exc.value.args[0]['records']) == 1
+        assert exc.value.args[0]['records'][0]['name'] == 'www.example.com'
+        assert exc.value.args[0]['records'][0]['done'] is False
+        assert 'values' not in exc.value.args[0]['records'][0]
+        assert exc.value.args[0]['records'][0]['check_count'] == 0
+
+    def test_cname_loop(self):
+        resolver = mock_resolver(['1.1.1.1'], {
+            ('1.1.1.1', ): [
+                {
+                    'target': 'ns.com',
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'ns.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.A, '2.2.2.2'),
+                    )),
+                },
+                {
+                    'target': 'ns.example.com',
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'ns.example.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.A, '3.3.3.3'),
+                    )),
+                },
+                {
+                    'target': 'ns.org',
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'ns.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.A, '2.2.3.3'),
+                    )),
+                },
+                {
+                    'target': 'ns.example.org',
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'ns.example.org',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.A, '4.4.4.4'),
+                    )),
+                },
+            ],
+        })
+        udp_sequence = [
+            {
+                'query_target': dns.name.from_unicode(u'com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, answer=[dns.rrset.from_rdata(
+                    'com',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.NS, 'ns.com'),
+                )]),
+            },
+            {
+                'query_target': dns.name.from_unicode(u'example.com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, answer=[dns.rrset.from_rdata(
+                    'example.com',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.NS, 'ns.example.com'),
+                )], cname=dns.name.from_unicode(u'example.com')),
+            },
+            {
+                'query_target': dns.name.from_unicode(u'www.example.com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, answer=[dns.rrset.from_rdata(
+                    'www.example.com',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.SOA, 'ns.example.com. ns.example.com. 12345 7200 120 2419200 10800'),
+                )], cname=dns.name.from_unicode(u'example.org')),
+            },
+            {
+                'query_target': dns.name.from_unicode(u'org'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, answer=[dns.rrset.from_rdata(
+                    'org',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.NS, 'ns.org'),
+                )]),
+            },
+            {
+                'query_target': dns.name.from_unicode(u'example.org'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, answer=[dns.rrset.from_rdata(
+                    'example.org',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.NS, 'ns.example.org'),
+                )], cname=dns.name.from_unicode(u'www.example.com')),
+            },
+        ]
+        with patch('dns.resolver.get_default_resolver', resolver):
+            with patch('dns.resolver.Resolver', resolver):
+                with patch('dns.query.udp', mock_query_udp(udp_sequence)):
+                    with patch('time.sleep', mock_sleep):
+                        with pytest.raises(AnsibleFailJson) as exc:
+                            set_module_args({
+                                'records': [
+                                    {
+                                        'name': 'www.example.com',
+                                        'values': [
+                                            'asdf',
+                                        ],
+                                    },
+                                ],
+                            })
+                            wait_for_txt.main()
+
+        print(exc.value.args[0])
+        assert exc.value.args[0]['msg'] == 'Unexpected resolving error: Found CNAME loop starting at www.example.com'
+        assert exc.value.args[0]['completed'] == 0
+        assert len(exc.value.args[0]['records']) == 1
+        assert exc.value.args[0]['records'][0]['name'] == 'www.example.com'
+        assert exc.value.args[0]['records'][0]['done'] is False
+        assert 'values' not in exc.value.args[0]['records'][0]
+        assert exc.value.args[0]['records'][0]['check_count'] == 0
