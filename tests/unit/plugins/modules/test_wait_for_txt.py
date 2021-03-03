20942669864d90162a64e260b296e558608d4715
Felix Fontein <felix@fontein.de>
2021-03-03 08:31:05 +0100

    Add timeout test.

--- tests/unit/plugins/modules/test_wait_for_txt.py
+++ tests/unit/plugins/modules/test_wait_for_txt.py
@@ -16,7 +16,7 @@ from ansible_collections.community.internal_test_tools.tests.unit.plugins.module
     set_module_args,
     ModuleTestCase,
     AnsibleExitJson,
-    # AnsibleFailJson,
+    AnsibleFailJson,
 )
 
 from ansible_collections.felixfontein.tools.plugins.modules import wait_for_txt
@@ -34,6 +34,16 @@ dns = pytest.importorskip('dns')
 
 def mock_sleep(delay):
     pass
+
+
+def mock_monotonic(call_sequence):
+    def f():
+        assert len(call_sequence) > 0, 'monotonic() was called more often than expected'
+        value = call_sequence[0]
+        del call_sequence[0]
+        return value
+
+    return f
 
 
 class TestWaitForTXT(ModuleTestCase):
@@ -979,3 +989,164 @@ class TestWaitForTXT(ModuleTestCase):
         assert exc.value.args[0]['records'][0]['done'] is True
         assert exc.value.args[0]['records'][0]['values'] == ['foo', 'bumble bee', 'wizard']
         assert exc.value.args[0]['records'][0]['check_count'] == 4
+
+    def test_timeout(self):
+        resolver = mock_resolver(['1.1.1.1'], {
+            ('1.1.1.1', ): [
+                {
+                    'target': 'ns.com',
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'ns.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.A, '2.2.2.2'),
+                    )),
+                },
+                {
+                    'target': 'ns.example.com',
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'ns.example.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.A, '3.3.3.3'),
+                    )),
+                },
+            ],
+            ('3.3.3.3', ): [
+                {
+                    'target': dns.name.from_unicode(u'www.example.com'),
+                    'rdtype': dns.rdatatype.TXT,
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'www.example.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.TXT, 'fdsa'),
+                    )),
+                },
+                {
+                    'target': dns.name.from_unicode(u'mail.example.com'),
+                    'rdtype': dns.rdatatype.TXT,
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'mail.example.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.TXT, '"any bar"'),
+                    )),
+                },
+                {
+                    'target': dns.name.from_unicode(u'www.example.com'),
+                    'rdtype': dns.rdatatype.TXT,
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'www.example.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.TXT, 'fdsa'),
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.TXT, 'asdf'),
+                    )),
+                },
+                {
+                    'target': dns.name.from_unicode(u'www.example.com'),
+                    'rdtype': dns.rdatatype.TXT,
+                    'lifetime': 10,
+                    'result': create_mock_answer(dns.rrset.from_rdata(
+                        'www.example.com',
+                        300,
+                        dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.TXT, 'asdfasdf'),
+                    )),
+                },
+            ],
+        })
+        udp_sequence = [
+            {
+                'query_target': dns.name.from_unicode(u'com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, answer=[dns.rrset.from_rdata(
+                    'com',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.NS, 'ns.com'),
+                )]),
+            },
+            {
+                'query_target': dns.name.from_unicode(u'example.com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, answer=[dns.rrset.from_rdata(
+                    'example.com',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.NS, 'ns.example.com'),
+                )], cname=dns.name.from_unicode(u'example.com')),
+            },
+            {
+                'query_target': dns.name.from_unicode(u'www.example.com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, authority=[dns.rrset.from_rdata(
+                    'www.example.com',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.SOA, 'ns.example.com. ns.example.com. 12345 7200 120 2419200 10800'),
+                )]),
+            },
+            {
+                'query_target': dns.name.from_unicode(u'mail.example.com'),
+                'query_type': dns.rdatatype.NS,
+                'nameserver': '1.1.1.1',
+                'kwargs': {
+                    'timeout': 10,
+                },
+                'result': create_mock_response(dns.rcode.NOERROR, authority=[dns.rrset.from_rdata(
+                    'mail.example.com',
+                    3600,
+                    dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.SOA, 'ns.example.com. ns.example.com. 12345 7200 120 2419200 10800'),
+                )]),
+            },
+        ]
+        with patch('dns.resolver.get_default_resolver', resolver):
+            with patch('dns.resolver.Resolver', resolver):
+                with patch('dns.query.udp', mock_query_udp(udp_sequence)):
+                    with patch('time.sleep', mock_sleep):
+                        with patch('ansible_collections.felixfontein.tools.plugins.modules.wait_for_txt.monotonic', mock_monotonic([0, 0.01, 1.2, 6.013, 7.41, 12.021])):
+                            with pytest.raises(AnsibleFailJson) as exc:
+                                set_module_args({
+                                    'records': [
+                                        {
+                                            'name': 'www.example.com',
+                                            'values': [
+                                                'asdf',
+                                            ],
+                                            'mode': 'equals',
+                                        },
+                                        {
+                                            'name': 'mail.example.com',
+                                            'values': [
+                                                'foo bar',
+                                                'any bar',
+                                            ],
+                                            'mode': 'superset',
+                                        },
+                                    ],
+                                    'timeout': 12,
+                                })
+                                wait_for_txt.main()
+
+        print(exc.value.args[0])
+        assert exc.value.args[0]['msg'] == 'Timeout (1 out of 2 check(s) passed).'
+        assert exc.value.args[0]['completed'] == 1
+        assert len(exc.value.args[0]['records']) == 2
+        assert exc.value.args[0]['records'][0]['name'] == 'www.example.com'
+        assert exc.value.args[0]['records'][0]['done'] is False
+        assert exc.value.args[0]['records'][0]['values'] == ['asdfasdf']
+        assert exc.value.args[0]['records'][0]['check_count'] == 3
+        assert exc.value.args[0]['records'][1]['name'] == 'mail.example.com'
+        assert exc.value.args[0]['records'][1]['done'] is True
+        assert exc.value.args[0]['records'][1]['values'] == ['any bar']
+        assert exc.value.args[0]['records'][1]['check_count'] == 1
