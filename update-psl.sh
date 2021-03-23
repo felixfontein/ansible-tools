0bd6c2d852fe4728208c806243ec396250dcd86b
Felix Fontein <felix@fontein.de>
2021-03-23 07:33:55 +0100

    Show differences in update-psl.sh.

--- update-psl.sh
+++ update-psl.sh
@@ -6,5 +6,6 @@ curl https://publicsuffix.org/list/public_suffix_list.dat --output plugins/publi
 git status plugins/public_suffix_list.dat
 
 if [ -n "$(git status --porcelain=v1 plugins/public_suffix_list.dat)" ]; then
+    git diff
     exit 1
 fi
